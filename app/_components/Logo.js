c3f937559b18460fc2230f4b831337e0a1851d24
Abdullah Alioglu <aliogluuabdullah@gmail.com>
2025-03-19 15:42:41 +0300

    fix : fix Logo component

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -5,7 +5,14 @@ import Image from "next/image";
 export default function Logo() {
   return (
     <Link href="/dashboard" className="flex items-center gap-2">
-      <Image src="/logo.png" alt="logo" width={60} height={60} quality={100} />
+      <Image
+        src="/logo.png"
+        alt="logo"
+        width={60}
+        height={60}
+        quality={100}
+        priority
+      />
       <span className="text-xl font-semibold text-primary-100">
         Electro Shop Admin
       </span>
