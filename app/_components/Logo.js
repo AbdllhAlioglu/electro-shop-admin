8c5b2417f8111325628e26282d66c1f7844c123e
Abdullah Alioglu <aliogluuabdullah@gmail.com>
2025-03-23 19:10:56 +0300

    fix: the image vercel

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,14 +1,16 @@
 import Link from "next/link";
+import Image from "next/image";
 import React from "react";
 
 export default function Logo() {
   return (
     <Link href="/dashboard" className="flex items-center gap-2">
-      <img
+      <Image
         src="/logo.png"
         alt="logo"
         width={60}
         height={60}
+        unoptimized
         className="object-contain"
       />
       <span className="text-xl font-semibold text-primary-100">
