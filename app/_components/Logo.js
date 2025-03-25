27c7cf1ee9474891011f2e1bc3701ff673d1355b
Abdullah Alioglu <aliogluuabdullah@gmail.com>
2025-03-25 11:30:58 +0300

    fix: fix the logo and added image in  storage

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -9,7 +9,7 @@ export default function Logo() {
       className="flex flex-col md:flex-row items-center gap-1 md:gap-2 p-2 md:p-0"
     >
       <Image
-        src="/logo.png"
+        src={`${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/images/logo.png`}
         alt="logo"
         width={60}
         height={60}
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [],
+    domains: [process.env.NEXT_PUBLIC_SUPABASE_PROJECT_ID + ".supabase.co"],
   },
 };
 
